44063babcf30bf9d003db9f667eacc87fea7121b
Niklas Brännlund <niklas.brannlund@live.se>
2019-03-11 11:19:49 +0100

    grouping data and printing result

--- assignment2/corrupted-currency.py
+++ assignment2/corrupted-currency.py
@@ -1,6 +1,10 @@
 import numpy as np
 from algorithm import kmeansclustering as kmc
+import pandas as pd
 import itertools as it
+
+MAX_PRICE = 20000
+MIN_PRICE = 0
 
 data = np.array([7845, 778, 942, 143, 0.75,
                  7956, 810, 976, 146, 0.76,
@@ -11,18 +15,27 @@ data = np.array([7845, 778, 942, 143, 0.75,
                  138, 0.82, 8219, 827, 995,
                  158, 0.82, 7500, 745, 948,
                  135, 0.67, 9257, 901, 120967,
-                 1154, 148, 0.72, 8553, 811,
+                 1154, 148, 0.72, 8553, 811,    
                  1218, 175, 0.84])
 
-def getKey(clusterdata):
-    return clusterdata.get_centroid().get_value()  
+def groupAndPrintData(result):
+    print("\r\n\r\n\r\n-----------------PRINTING RESULT----------------\r\n")
+    
+    df = pd.DataFrame({'key': [r.get_centroid().get_value() for r in result],
+                  'data': [r.value for r in result]}, columns=['key', 'data'])
+    grouped_df = df.groupby(df.key)
+    
+    # maximum prices
+    maxKey = grouped_df.keys.max()
+    print("------MAX PRICES-----\r\n")
+    print(grouped_df.get_group(maxKey), "\r\n")
+
+    # min prices
+    minKey = grouped_df.keys.min()
+    print("------MIN PRICES-----\r\n")
+    print(grouped_df.get_group(minKey), "\r\n")
+
 
 k = kmc.KMeansClustering(data, 6)
 result = k.execute()
-
-sortedList = sorted(result, key=getKey)
-print("------ Min Price ------")
-
-
-for r in sorted(result, key=getKey):
-    print("Value: {}, Centroid: {}".format(r.value, r.get_centroid().get_value()))
+groupAndPrintData(result)
