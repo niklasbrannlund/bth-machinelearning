1b4461b35177f92358966e53793a71e2827a0ee9
Niklas Brännlund <niklas.brannlund@live.se>
2019-03-24 14:31:35 +0100

    changed learning rate of neural network

--- assignment2/perceptron-neural-network.py
+++ assignment2/perceptron-neural-network.py
@@ -2,7 +2,7 @@ import numpy as np
 from algorithm import neuralnetwork as nn
 
 weights = np.array([0.5, 0.1]).reshape(1,2) # initial weights
-learning_rate = 0.001
+learning_rate = 0.01
 bias = -0.1
 perceptron = nn.NeuralNetwork(weights, learning_rate, bias)
 perceptron.execute()
